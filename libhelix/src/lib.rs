7ff1e33769c03d8c883fbc3525de4464d3e2d729
leocth <leocth31@gmail.com>
2022-10-20 12:11:49 +0800

    feat: generate static libs, add `cbindgen` for generating headers

--- libhelix/src/lib.rs
+++ libhelix/src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[no_mangle]
+pub extern "C" fn test(i: std::ffi::c_int) -> std::ffi::c_int {
+    i + 1
 }
