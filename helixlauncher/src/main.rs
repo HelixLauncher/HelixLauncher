9cb99395a4ae33a25d4ef18871a4c3e4a1767566
leo60228 <leo@60228.dev>
2022-10-21 15:27:46 -0400

    default to imagine if style not overridden by env var

--- helixlauncher/src/main.rs
+++ helixlauncher/src/main.rs
@@ -1,4 +1,6 @@
 use qmetaobject::prelude::*;
+use qmetaobject::qtquickcontrols2::QQuickStyle;
+use std::env;
 
 qrc!(register_resources,
      "qml" as "helixlauncher/qml" {
@@ -8,6 +10,10 @@ qrc!(register_resources,
 
 fn main() {
     register_resources();
+
+    if env::var_os("QT_QUICK_CONTROLS_STYLE").is_none() {
+        QQuickStyle::set_style("Imagine");
+    }
 
     let mut engine = QmlEngine::new();
     engine.load_file("qrc:/helixlauncher/qml/main.qml".into());
