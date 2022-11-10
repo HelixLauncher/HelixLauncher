73344e437f5a5d13ceda486dc6ab80793ab59cdf
jo! <me@j0.lol>
2022-11-10 14:28:48 +0000

    core: add basic account manager api

--- helixlauncher-core/src/auth/account.rs
+++ helixlauncher-core/src/auth/account.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::{fs::File, io::BufReader, path::Path};
 
 #[derive(Serialize, Deserialize)]
 pub struct Account {
@@ -6,4 +7,18 @@ pub struct Account {
     pub username: String,
     pub refresh_token: String,
     pub token: String,
+}
+
+pub fn get_accounts(account_json: &Path) -> Vec<Account> {
+    serde_json::from_reader(BufReader::new(File::open(account_json).unwrap())).unwrap()
+}
+
+pub fn add_account(account: Account, account_json: &Path) {
+    let mut accounts: Vec<Account> =
+        serde_json::from_reader(BufReader::new(File::open(account_json).unwrap())).unwrap();
+
+    accounts.push(account);
+
+    let writer = File::create(account_json).unwrap();
+    serde_json::to_writer_pretty(writer, &accounts).unwrap();
 }
