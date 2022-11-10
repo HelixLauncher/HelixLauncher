2c35b68a61074d79ea17519208a98894deef5e80
jo! <me@j0.lol>
2022-11-10 16:32:09 +0000

    core: add Error struct to account manager

--- helixlauncher-core/src/auth/account.rs
+++ helixlauncher-core/src/auth/account.rs
@@ -1,5 +1,15 @@
 use serde::{Deserialize, Serialize};
 use std::{fs::File, io::BufReader, path::Path};
+use thiserror::Error;
+
+#[derice(Error, Debug)]
+pub enum AccountManagerError {
+    #[error(Transparent)]
+    ParseError(#[from] serde_json::Error),
+
+    #[error(Transparent)]
+    FileError(#[from] std::io::Error),
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct Account {
@@ -9,16 +19,18 @@ pub struct Account {
     pub token: String,
 }
 
-pub fn get_accounts(account_json: &Path) -> Vec<Account> {
-    serde_json::from_reader(BufReader::new(File::open(account_json).unwrap())).unwrap()
+pub fn get_accounts(account_json: &Path) -> Result<Vec<Account>, AccountManagerError> {
+    serde_json::from_reader(BufReader::new(File::open(account_json)?))?
 }
 
-pub fn add_account(account: Account, account_json: &Path) {
+pub fn add_account(account: Account, account_json: &Path) -> Result<()> {
     let mut accounts: Vec<Account> =
-        serde_json::from_reader(BufReader::new(File::open(account_json).unwrap())).unwrap();
+        serde_json::from_reader(BufReader::new(File::open(account_json)?))?;
 
     accounts.push(account);
 
-    let writer = File::create(account_json).unwrap();
-    serde_json::to_writer_pretty(writer, &accounts).unwrap();
+    let writer = File::create(account_json)?;
+    serde_json::to_writer_pretty(writer, &accounts)?;
+
+    Ok(())
 }
