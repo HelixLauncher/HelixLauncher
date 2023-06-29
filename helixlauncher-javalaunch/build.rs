cf368e5b8f008454d04dfbc3398e80f55d9809f1
kb1000 <kaeptmblaubaer1000@gmail.com>
2023-06-29 14:55:55 +0200

    Update dependencies

--- helixlauncher-javalaunch/build.rs
+++ helixlauncher-javalaunch/build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    embed_resource::compile("src/helixlauncher.rc");
+    embed_resource::compile("src/helixlauncher.rc", embed_resource::NONE);
     cc::Build::new().file("src/exports.c").compile("exports");
 }
