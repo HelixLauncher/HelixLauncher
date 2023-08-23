bc4162b50baf8b844433c16cfe506fefbc961149
anonymous123-code <61744596+anonymous123-code@users.noreply.github.com>
2023-08-23 20:26:33 +0200

    Introduce testing - fix component_version_exists requiring String instead of &str - fix get_component_index using the wrong url

--- helixlauncher-core/src/meta.rs
+++ helixlauncher-core/src/meta.rs
@@ -73,7 +73,10 @@ impl<'a> MetaClient<'a> {
     ) -> Result<helixlauncher_meta::index::Index, ComponentMetaRetrievalError> {
         let response = self
             .client
-            .get(format!("{}{component_id}.json", self.config.get_meta_url()))
+            .get(format!(
+                "{}{component_id}/index.json",
+                self.config.get_meta_url()
+            ))
             .send()
             .await?
             .error_for_status();
@@ -94,8 +97,8 @@ impl<'a> MetaClient<'a> {
 
     pub async fn component_version_exists(
         &self,
-        component_id: String,
-        component_version: String,
+        component_id: &str,
+        component_version: &str,
     ) -> Result<bool, ComponentMetaRetrievalError> {
         Ok(self
             .get_component_index(&component_id)
@@ -117,4 +120,215 @@ pub enum ComponentMetaRetrievalError {
     VersionNotFound { id: String, version: String },
     #[error("Component {id} not found")]
     IndexNotFound { id: String },
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::config::Config;
+
+    use super::*;
+
+    const VALID_TEST_SET: [(&str, [&str; 3]); 1] = [
+        // TODO: adjust to meta changes
+        ("net.minecraft", ["1.16.5", "23w18a", "3D Shareware v1.34"]),
+        // ("org.quiltmc.quilt-loader", ["0.20.0-beta.2", "0.18.9", "0.18.1-beta.10"]),
+        // ("net.fabricmc.fabric-loader", ["", "", ""]),
+        // ("net.minecraftforge.forge", ["", "", ""]),
+    ];
+
+    #[tokio::test]
+    async fn get_component_meta() -> Result<(), Box<dyn std::error::Error>> {
+        let dir = tempfile::tempdir()?;
+        let config = Config::new_with_data_dir(
+            "dev.helixlauncher.HelixLauncher",
+            "HelixLauncher",
+            dir.path().join("abc"),
+        )?;
+
+        for (component_id, versions) in VALID_TEST_SET {
+            for component_version in versions {
+                MetaClient::new(&config)
+                    .get_component_meta(component_id, component_version)
+                    .await?;
+            }
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn get_component_meta_same_client() -> Result<(), Box<dyn std::error::Error>> {
+        let dir = tempfile::tempdir()?;
+        let config = Config::new_with_data_dir(
+            "dev.helixlauncher.HelixLauncher",
+            "HelixLauncher",
+            dir.path().join("abc"),
+        )?;
+        let client = MetaClient::new(&config);
+        for (component_id, versions) in VALID_TEST_SET {
+            for component_version in versions {
+                client
+                    .get_component_meta(component_id, component_version)
+                    .await?;
+            }
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn get_component_index() -> Result<(), Box<dyn std::error::Error>> {
+        let dir = tempfile::tempdir()?;
+        let config = Config::new_with_data_dir(
+            "dev.helixlauncher.HelixLauncher",
+            "HelixLauncher",
+            dir.path().join("abc"),
+        )?;
+
+        for (component_id, _) in VALID_TEST_SET {
+            assert!(
+                MetaClient::new(&config)
+                    .get_component_index(component_id)
+                    .await?
+                    .len()
+                    > 5
+            );
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn get_component_index_same_client() -> Result<(), Box<dyn std::error::Error>> {
+        let dir = tempfile::tempdir()?;
+        let config = Config::new_with_data_dir(
+            "dev.helixlauncher.HelixLauncher",
+            "HelixLauncher",
+            dir.path().join("abc"),
+        )?;
+        let client = MetaClient::new(&config);
+        for (component_id, _) in VALID_TEST_SET {
+            assert!(client.get_component_index(component_id).await?.len() > 5);
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn component_version_exists() -> Result<(), Box<dyn std::error::Error>> {
+        let dir = tempfile::tempdir()?;
+        let config = Config::new_with_data_dir(
+            "dev.helixlauncher.HelixLauncher",
+            "HelixLauncher",
+            dir.path().join("abc"),
+        )?;
+
+        for (component_id, versions) in VALID_TEST_SET {
+            for component_version in versions {
+                assert!(
+                    MetaClient::new(&config)
+                        .component_version_exists(component_id, component_version)
+                        .await?
+                )
+            }
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn component_version_exists_same_client() -> Result<(), Box<dyn std::error::Error>> {
+        let dir = tempfile::tempdir()?;
+        let config = Config::new_with_data_dir(
+            "dev.helixlauncher.HelixLauncher",
+            "HelixLauncher",
+            dir.path().join("abc"),
+        )?;
+
+        let client = MetaClient::new(&config);
+        for (component_id, versions) in VALID_TEST_SET {
+            for component_version in versions {
+                assert!(
+                    client
+                        .component_version_exists(component_id, component_version)
+                        .await?
+                )
+            }
+        }
+
+        Ok(())
+    }
+
+    const INVALID_TEST_SET: [(&str, [&str; 3]); 2] = [
+        // first should have invalid index, rest only invalid versions
+        ("com.example", ["1.0.0", "1.0.1", "1.0.2"]),
+        ("net.minecraft", ["4.0.0", "aklsnbafoibesgobo", "100+5=3"]),
+    ];
+
+    #[tokio::test]
+    async fn get_component_meta_invalid_version() -> Result<(), Box<dyn std::error::Error>> {
+        let dir = tempfile::tempdir()?;
+        let config = Config::new_with_data_dir(
+            "dev.helixlauncher.HelixLauncher",
+            "HelixLauncher",
+            dir.path().join("abc"),
+        )?;
+
+        for (component_id, versions) in INVALID_TEST_SET {
+            for component_version in versions {
+                assert!(matches!(
+                    MetaClient::new(&config)
+                        .get_component_meta(component_id, component_version)
+                        .await,
+                    Err(ComponentMetaRetrievalError::VersionNotFound {
+                        id,
+                        version
+                    }) if id == component_id && version == component_version
+                ));
+            }
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn get_component_index_invalid_id() -> Result<(), Box<dyn std::error::Error>> {
+        let dir = tempfile::tempdir()?;
+        let config = Config::new_with_data_dir(
+            "dev.helixlauncher.HelixLauncher",
+            "HelixLauncher",
+            dir.path().join("abc"),
+        )?;
+
+        assert!(matches!(
+            MetaClient::new(&config)
+                .get_component_index(INVALID_TEST_SET[0].0)
+                .await,
+            Err(
+                ComponentMetaRetrievalError::IndexNotFound{
+                id
+            }) if id == INVALID_TEST_SET[0].0
+        ));
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn get_component_meta_invalid_formatting() -> Result<(), Box<dyn std::error::Error>> {
+        let dir = tempfile::tempdir()?;
+        let config = Config::new_with_data_dir(
+            "dev.helixlauncher.HelixLauncher",
+            "HelixLauncher",
+            dir.path().join("abc"),
+        )?;
+
+        assert!(matches!(
+            MetaClient::new(&config)
+                .get_component_meta("net.minecraft", "index")
+                .await,
+            Err(ComponentMetaRetrievalError::ParseError(_))
+        ));
+
+        Ok(())
+    }
 }
